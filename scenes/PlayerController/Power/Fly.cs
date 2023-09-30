8211a56746a63f6a51d3e295cae1b5bd567dec89
Marvin Mottet <11558282+mottet@users.noreply.github.com>
2023-09-30 03:54:30 +0200

    start fly mecanics

--- scenes/PlayerController/Power/Fly.cs
+++ scenes/PlayerController/Power/Fly.cs
@@ -4,12 +4,12 @@ using LudumDare54Silver.scenes.PlayerController.Power;
 public partial class Fly : Node3D, Power
 {
     [Export]
-    public float Speed = 5.0f;
+    public float Speed = 20.0f;
     [Export]
-    public float JumpVelocity = 12f;
+    public float JumpVelocity = 6f;
 
     // Get the gravity from the project settings to be synced with RigidBody nodes.
-    public float gravity = ProjectSettings.GetSetting("physics/3d/default_gravity").AsSingle() * 5;
+    public float gravity = ProjectSettings.GetSetting("physics/3d/default_gravity").AsSingle() * 2;
 
     public void MoveCharacter(CharacterBody3D character, double delta)
     {
@@ -18,22 +18,21 @@ public partial class Fly : Node3D, Power
         // Add the gravity.
         if (!character.IsOnFloor())
             velocity.Y -= gravity * (float)delta;
-
-        // Handle Jump.
-        if (Input.IsActionJustPressed("ui_accept"))
-            velocity.Y = JumpVelocity;
-
-        // Get the input direction and handle the movement/deceleration.
-        // As good practice, you should replace UI actions with custom gameplay actions.
-        Vector2 inputDir = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-        Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
-        if (direction != Vector3.Zero)
+    
+        bool flapLeft = Input.IsActionJustReleased("ui_left");
+        bool flapRight = Input.IsActionJustReleased("ui_right");
+        if (flapLeft || flapRight)
         {
-            velocity.X = direction.X * Speed;
+            velocity.Y = flapLeft && flapRight ? JumpVelocity * 2 : JumpVelocity;
+        }
+        if ((flapLeft && !flapRight) || (!flapLeft && flapRight))
+        {
+            var direction = flapLeft ? 1 : -1;
+            velocity.X = direction * Speed;
         }
         else
         {
-            velocity.X = Mathf.MoveToward(character.Velocity.X, 0, Speed);
+            velocity.X = Mathf.MoveToward(character.Velocity.X, 0, Speed / 2);
         }
 
         character.Velocity = velocity;
