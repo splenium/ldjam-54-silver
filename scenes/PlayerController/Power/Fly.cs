230ed6b2468c9fbfc28b6d0a2e057d41bb1411f9
Nougato <unconfigured@null.spigotmc.org>
2023-10-01 01:40:51 +0200

    add play sound on character boost (troll sound) + fix script random

--- scenes/PlayerController/Power/Fly.cs
+++ scenes/PlayerController/Power/Fly.cs
@@ -18,12 +18,17 @@ public partial class Fly : Power
     public CsgMesh3D WingRight;
     [Export]
     public CsgMesh3D WingLeft;
+    [Export]
+    public AudioStreamPlayer BoostSound;
+    [Export]
+    public AudioStreamMP3[] BoostSounds;
 
-    private WingsMouvement wingsPosition = new WingsMouvement();
+    private WingsMouvement wingsPosition = null;
 
     public override void _Ready()
     {
         base._Ready();
+        wingsPosition = new WingsMouvement(this);
         Visible = false;
         AnimationLeftWing.Play("left_wing_down");
         AnimationRightWing.Play("right_wing_down");
@@ -77,6 +82,18 @@ public partial class Fly : Power
 
         SetRaKoonAvatarAnimation(velocity);
     }
+
+    public void PlayBoostSound()
+    {
+        if (BoostSound.Playing)
+        {
+            return;
+        }
+        AudioStreamMP3 RngBoostSound = BoostSounds[GD.Randi() % BoostSounds.Length];
+        BoostSound.Stream = RngBoostSound;
+        BoostSound.Play();
+    }
+
 }
 
 class WingsMouvement
@@ -89,6 +106,12 @@ class WingsMouvement
     private long whenLeftWingFlapDown;
     private long whenRightWingFlapDown;
     private long deltaInMillisecondToAcceptUpBoost = 100;
+    private Fly parent;
+
+    public WingsMouvement(Fly parent)
+    {
+        this.parent = parent;
+    }
 
     public Vector2 Update()
     {
@@ -127,6 +150,10 @@ class WingsMouvement
         if (LeftWing == WingMovement.Down || RightWing == WingMovement.Down)
         {
             direction.Y = hasUpBoost ? 2 : 1;
+            if (hasUpBoost)
+            {
+                parent.PlayBoostSound();
+            }
         }
         if (OnlyOneWingFlap())
         {
