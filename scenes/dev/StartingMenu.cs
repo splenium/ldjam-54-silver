1609c0723836a7c376cf8b6d91d95164d6337ea5
Herbigniaux <b.herbigniaux@hotmail.fr>
2023-10-02 19:54:55 +0200

    Add menu and credit scene

--- scenes/dev/StartingMenu.cs
+++ scenes/dev/StartingMenu.cs
@@ -6,8 +6,41 @@ public partial class StartingMenu : Control
 	[Export]
 	private PackedScene StartingScene;
 
+	[Export]
+	private Control CreditsControl;
+	[Export]
+	private BoxContainer BoxContainer;
+
 	public void _on_start_button_pressed()
 	{
 		GetTree().ChangeSceneToFile(StartingScene.ResourcePath);
 	}
+
+	private void _on_credits_button_pressed()
+	{
+		CreditsControl.Visible = true;
+		BoxContainer.Visible = false;
+
+	}
+	private void _on_back_button_pressed()
+	{
+		CreditsControl.Visible = false;
+		BoxContainer.Visible = true;
+
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
