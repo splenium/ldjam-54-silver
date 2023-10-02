b41bb39b97f08c0840190ada875a73210ada3e78
Herbigniaux <b.herbigniaux@hotmail.fr>
2023-10-02 19:56:11 +0200

    Set up correct starting level

--- assets/Scripts/Level.cs
+++ assets/Scripts/Level.cs
@@ -3,16 +3,16 @@ using Godot;
 public partial class Level : Node3D
 {
 
-    [Export]
-    public AudioStream LevelSound;
-    [Export]
-    public Checkpoint DefaultCheckpoint;
-    [Export]
-    public float VolumeDb = 0f;
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        GameManager.PlayMusic(LevelSound, VolumeDb);
-        GameManager.InitializeCheckpoint(DefaultCheckpoint);
-    }
+	[Export]
+	public AudioStream LevelSound;
+	[Export]
+	public Checkpoint DefaultCheckpoint;
+	[Export]
+	public float VolumeDb = 0f;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		GameManager.PlayMusic(LevelSound, VolumeDb);
+		GameManager.InitializeCheckpoint(DefaultCheckpoint);
+	}
 }
